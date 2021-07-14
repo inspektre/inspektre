a908ba66612aab94a69215d295bdc3d85ffbf746
Uday Korlimarla <44824134+udaykor@users.noreply.github.com>
2021-07-15 00:25:57 +1000

    save token and read token for generating JWT

--- src/utils/auth/index.ts
+++ src/utils/auth/index.ts
@@ -33,6 +33,10 @@ const startInspektreDevice = async (verbose: Boolean) => {
   });
 };
 
+// Get Device token with Device Authorization flow.
+// Flow is handled on inspektre to make this a turn-key solution.
+// Advantage is that client_secret and client_id are not shared.
+// Disadvantage: Device Authorization spec is violated.
 const getDeviceToken = async (verbose: Boolean, deviceCode: string, interval?: number) => {
   return await new Promise(async (resolve, reject) => {
     try {
@@ -52,24 +56,29 @@ const getDeviceToken = async (verbose: Boolean, deviceCode: string, interval?: n
   });
 };
 
-const readToken = async () => {
+// Read Device Token
+const readToken = () => {
   const data: Buffer = fs.readFileSync(credsFile);
-  return JSON.parse(data.toString());
+  const { token } = JSON.parse(data.toString()).token;
+  return token;
 };
 
-const saveToken = async (token: string) => {
+// Save Device Token
+const saveToken = (token: string) => {
   const data = { token, time: new Date().getTime() };
   fs.writeFileSync(credsFile, JSON.stringify(data).concat('\n'));
 };
 
-const getAccessToken = async(token: string) => {
+// Get Access Token (JWT) Generated from the Device Token
+// const jwt = await getAccessToken();
+export const getAccessToken = async() => {
   return await new Promise(async (resolve, reject) => {
     try {
       const options: any = {
         prefixUrl: 'https://inspektre.io/api/cli/accesstoken',
         headers: {
           'user-agent': '@inspektre/cli',
-          'x-inspektre-token': token,
+          'x-inspektre-token': readToken(),
         }
       };
       const resp: any = await got.post(options).json();
@@ -86,6 +95,7 @@ export const performDeviceLogin = async (headless: Boolean, verbose: Boolean) =>
   if(!headless) {
     await open(startDeviceAuth.verification_uri_complete);
     const deviceToken: any = await getDeviceToken(verbose, startDeviceAuth.device_code, startDeviceAuth.interval);
+    saveToken(deviceToken);
   } else { 
     console.log(`To Authorize this device, Please visit: ${startDeviceAuth.verification_uri_complete}`);
     setTimeout(async() => {
